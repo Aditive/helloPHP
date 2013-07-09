86f2f702fd326b993741518c0243c23fa7a58b72
kquinsland <karl+github@karlquinsland.com>
2013-07-09 15:50:16 -0700

    Update index.php
    test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-echo("hi, i am PHP (" . phpversion() . ").  ALL the info <a href=\"info.php\">here</a> <br> See if you can tell if i'm behind a load ballencer or not ;) ");
+echo("hi, i am PHP (" . phpversion() . ").  (and this is the new file!) ALL the info <a href=\"info.php\">here</a> <br> See if you can tell if i'm behind a load ballencer or not ;) ");
 
 echo("<br> HOSTNAME: " . gethostname()  . "<br>");
 
