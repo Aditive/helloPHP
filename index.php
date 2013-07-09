fc4df53fc144cc251981b5e57bf6f4dded61a798
kquinsland <karl+github@karlquinsland.com>
2013-07-09 15:47:19 -0700

    Update index.php
    added host info for ELB check

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo("hi, i am PHP.  details: <a href=\"info.php\">here</a>");
+
+echo("hi, i am PHP (" . phpversion() . ").  ALL the info <a href=\"info.php\">here</a> <br> See if you can tell if i'm behind a load ballencer or not ;) ");
+
+echo("<br> HOSTNAME: " . gethostname()  . "<br>");
+
 
 ?>
